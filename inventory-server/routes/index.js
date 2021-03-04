7382debda8fd0aa5ffaea6e5443d4281127336ad
root <vikas.singla86@gmail.com>
2021-03-04 17:12:13 +0530

    Changes in base_url

--- inventory-server/routes/index.js
+++ inventory-server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-  console.log("Upload page");
+  console.log("Upload page .. ");
   res.write("product list ... ");
 
 });
